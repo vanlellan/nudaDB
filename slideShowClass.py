9d34ab3a59d6faeb9d2da60359f540a8cdfe2cbd
Randall Evan McClellan <vanlellan@gmail.com>
2021-11-08 22:17:57 -0600

    add feature: sort import files by exif timestamp

--- slideShowClass.py
+++ slideShowClass.py
@@ -24,233 +24,256 @@ import datetime
 import pickle
 
 #TODO
-#	Make this class general enough to use for both import and slideshow
-	#for import, text input bar sets tags for the current image
-	#for slideshow, text input bar restarts slideshow with new search terms
+#    Make this class general enough to use for both import and slideshow
+    #for import, text input bar sets tags for the current image
+    #for slideshow, text input bar restarts slideshow with new search terms
+    #fix final skipped file in a batch of duplicates getting tag input anyway
+    #for import, sort inbox files by timestamp
 
-#NUDADBDIR = os.path.dirname(os.path.abspath(sys.argv[0])) + '/nudaDBDir/'		#this gets the directory of the python script
-NUDADBDIR = os.getcwd() + '/nudaDBDir/'							#this gets the current working directory
+#NUDADBDIR = os.path.dirname(os.path.abspath(sys.argv[0])) + '/nudaDBDir/'        #this gets the directory of the python script
+NUDADBDIR = os.getcwd() + '/nudaDBDir/'                            #this gets the current working directory
 #print NUDADBDIR
 #NUDADBTABLE = os.path.dirname(os.path.abspath(sys.argv[0])) + '/nudaDBTable.txt'
 NUDADBTABLE = os.getcwd() + '/nudaDBTable.txt'
 MONTHS = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
 
 def getImagesMatchingTags(listOfTags):
-	with open("tags.pickle","rb") as pickleFile:
-		tagDict = pickle.load(pickleFile)
-	imagelist = []
-	for tag in listOfTags:
-		for result in tagDict[tag]:
-			imagelist.append(result)
-	return imagelist
+    with open("tags.pickle","rb") as pickleFile:
+        tagDict = pickle.load(pickleFile)
+    imagelist = []
+    for tag in listOfTags:
+        for result in tagDict[tag]:
+            imagelist.append(result)
+    return imagelist
 
 class slideShowClass:
-	def __init__(self,master,listOfImagePaths,showOrImport):
-		self.master = master
-		self.listOfImagePaths = listOfImagePaths
-		self.showOrImport = showOrImport
-		self.input_strings = []
-		self.currentInputIndex = 0
-		self.afterID = None
-		self.currentImageIndex = -1
-		self.rotations = {3: 180, 6: 270, 8: 90}
-		#self.currentImage = ImageTk.PhotoImage(self.makeThumb(self.listOfImagePaths[0]))
-		self.currentImage = None
-		self.currentImageOriginal = None
-		self.fullscreenState = True
+    def __init__(self,master,listOfImagePaths,showOrImport):
+        self.master = master
+        self.listOfImagePaths = listOfImagePaths
+        self.showOrImport = showOrImport
+        self.input_strings = []
+        self.currentInputIndex = 0
+        self.afterID = None
+        self.currentImageIndex = -1
+        self.rotations = {3: 180, 6: 270, 8: 90}
+        #self.currentImage = ImageTk.PhotoImage(self.makeThumb(self.listOfImagePaths[0]))
+        self.currentImage = None
+        self.currentImageOriginal = None
+        self.fullscreenState = True
 
-		master.title("Slide Show")
-		master.geometry(str(round(0.9*master.winfo_screenwidth()))+'x'+str(round(0.9*master.winfo_screenheight())))
-		master.configure(background='black')
-		self.master.attributes("-fullscreen", self.fullscreenState)
+        self.sortImageListByTimeStamp()
 
-		self.showpanel = tk.Label(master, image=self.currentImage)
-		self.showpanel.pack(fill='both', expand='yes')
+        master.title("Slide Show")
+        master.geometry(str(round(0.9*master.winfo_screenwidth()))+'x'+str(round(0.9*master.winfo_screenheight())))
+        master.configure(background='black')
+        self.master.attributes("-fullscreen", self.fullscreenState)
 
-		self.textbox = tk.Entry(master)
-		self.textbox.focus()
-		if self.showOrImport == 'show':
-			self.textbox.bind("<Return>", self.new_search)
-			self.textbox.bind("<Next>", self.show_next)	#Page Down
-			#self.textbox.bind("<Prior>", self.show_prior)	#Page Up
-			self.textbox.bind("<Up>", self.input_hist_prev)
-			self.textbox.bind("<Down>", self.input_hist_next)
-		elif self.showOrImport == 'import':
-			self.textbox.bind("<Return>", self.send_tags)
-			self.textbox.bind("<Up>", self.input_hist_prev)
-			self.textbox.bind("<Down>", self.input_hist_next)
-		self.textbox.bind("<Control-Key-w>", self.show_stop)
-		self.textbox.bind("<Escape>", self.fullscreen_off)
-		self.textbox.bind("<F11>", self.toggle_fullscreen)
-		self.textbox.pack(side='bottom', fill='x', expand=True)
+        self.showpanel = tk.Label(master, image=self.currentImage)
+        self.showpanel.pack(fill='both', expand='yes')
 
-		if self.showOrImport == 'show':
-			self.show_next()
-		elif self.showOrImport == 'import':
-			if self.next_image():
-				self.tag_input()
+        self.textbox = tk.Entry(master)
+        self.textbox.focus()
+        if self.showOrImport == 'show':
+            self.textbox.bind("<Return>", self.new_search)
+            self.textbox.bind("<Next>", self.show_next)    #Page Down
+            #self.textbox.bind("<Prior>", self.show_prior)    #Page Up
+            self.textbox.bind("<Up>", self.input_hist_prev)
+            self.textbox.bind("<Down>", self.input_hist_next)
+        elif self.showOrImport == 'import':
+            self.textbox.bind("<Return>", self.send_tags)
+            self.textbox.bind("<Up>", self.input_hist_prev)
+            self.textbox.bind("<Down>", self.input_hist_next)
+        self.textbox.bind("<Control-Key-w>", self.show_stop)
+        self.textbox.bind("<Escape>", self.fullscreen_off)
+        self.textbox.bind("<F11>", self.toggle_fullscreen)
+        self.textbox.pack(side='bottom', fill='x', expand=True)
 
-	def fullscreen_off(self, event=None):
-		self.fullscreenState = False
-		self.master.attributes("-fullscreen", False)
+        if self.showOrImport == 'show':
+            self.show_next()
+        elif self.showOrImport == 'import':
+            if self.next_image():
+                self.tag_input()
 
-	def toggle_fullscreen(self, event=None):
-		self.fullscreenState = not self.fullscreenState
-		self.master.attributes("-fullscreen", self.fullscreenState)
+    def sortImageListByTimeStamp(self, event=None):
+        idummy = 1
+        tempList = list(self.listOfImagePaths)
+        stamps = []
+        for impath in tempList:
+            aImage = Image.open(impath)
+            try:
+                fullexif=aImage._getexif()
+                aTime = datetime.datetime.strptime(fullexif[36867], "%Y:%m:%d %H:%M:%S")
+                stamps.append(int(aTime.timestamp()))
+            except Exception as ex:
+                print("EXIF Problem! Assigning unix timestamp as ", idummy)
+                print(ex)
+                idummy = idummy + 1
+        sortedList = [x for _, x in sorted(zip(stamps,tempList), key=lambda pair: pair[0])]
+        self.listOfImagePaths = list(sortedList)
 
-	def new_search(self, event=None):
-		newTags = self.textbox.get()
-		self.input_strings.append(newTags)
-		taglist = self.input_strings[-1].split(' ')
-		self.listOfImagePaths = getImagesMatchingTags(taglist)
-		self.currentImageIndex = -1
-		self.show_next()
-		self.currentInputIndex = 0
-		self.textbox.delete(0, tk.END)
+    def fullscreen_off(self, event=None):
+        self.fullscreenState = False
+        self.master.attributes("-fullscreen", False)
 
-	def getHash(self, thefile):
-		BLOCKSIZE = 65536
-		hasher = hashlib.md5()
-		with open(thefile, 'rb') as afile:
-			buf = afile.read(BLOCKSIZE)
-			while len(buf) > 0:
-				hasher.update(buf)
-				buf = afile.read(BLOCKSIZE)
-		return hasher.hexdigest()
+    def toggle_fullscreen(self, event=None):
+        self.fullscreenState = not self.fullscreenState
+        self.master.attributes("-fullscreen", self.fullscreenState)
 
-	def input_hist_prev(self, event=None):
-		self.currentInputIndex -= 1
-		if self.currentInputIndex >= -len(self.input_strings):
-			self.textbox.delete(0, tk.END)
-			self.textbox.insert(0, self.input_strings[self.currentInputIndex])
-		else:
-			self.currentInputIndex += 1
+    def new_search(self, event=None):
+        newTags = self.textbox.get()
+        self.input_strings.append(newTags)
+        taglist = self.input_strings[-1].split(' ')
+        self.listOfImagePaths = getImagesMatchingTags(taglist)
+        self.currentImageIndex = -1
+        self.show_next()
+        self.currentInputIndex = 0
+        self.textbox.delete(0, tk.END)
 
-	def input_hist_next(self, event=None):
-		self.currentInputIndex += 1
-		if self.currentInputIndex < 0:
-			self.textbox.delete(0, tk.END)
-			self.textbox.insert(0, self.input_strings[self.currentInputIndex])
-		else:
-			self.currentInputIndex = 0
-			self.textbox.delete(0, tk.END)
+    def getHash(self, thefile):
+        BLOCKSIZE = 65536
+        hasher = hashlib.md5()
+        with open(thefile, 'rb') as afile:
+            buf = afile.read(BLOCKSIZE)
+            while len(buf) > 0:
+                hasher.update(buf)
+                buf = afile.read(BLOCKSIZE)
+        return hasher.hexdigest()
 
-	def send_tags(self, event=None):
-		self.currentInputIndex = 0
-		newTags = self.textbox.get()
-		if newTags in ['\\quit', '\\exit', '\\abort']:
-			self.master.quit()
-			return None
-		else:
-			self.input_strings.append(newTags)
-		taglist = self.input_strings[-1].split(' ')
-		tags = ','.join(taglist)
-		try:
-			os.system("cp "+self.fullpath.replace(' ', "\ ")+" "+NUDADBDIR+self.month+str(self.dateAndTime.year)+'/'+self.newName)
-			#Add entry to table
-			with open(NUDADBTABLE, 'a') as table:
-				table.write(self.newName+'\t'+'./nudaDBDir/'+self.month+str(self.dateAndTime.year)+'/'+'\t'+self.dateAndTime.strftime("%Y-%m-%d\t%H:%M:%S")+'\t'+tags+'\n')
-			#if using default import, move file from ./inbox/ to ./inbox/imported/
-			if os.path.isfile('./inbox/'+self.filename):
-				os.system("mv "+self.fullpath.replace(' ', "\ ")+" "+NUDADBDIR+"../inbox/imported/"+self.newName)
-		except:
-			print("copy problem!")
-		self.textbox.delete(0, tk.END)
-		if self.next_image():
-			self.tag_input()
+    def input_hist_prev(self, event=None):
+        self.currentInputIndex -= 1
+        if self.currentInputIndex >= -len(self.input_strings):
+            self.textbox.delete(0, tk.END)
+            self.textbox.insert(0, self.input_strings[self.currentInputIndex])
+        else:
+            self.currentInputIndex += 1
 
-	def tag_input(self, event=None):
-		#Get file data
-		self.fullpath = os.path.abspath(self.listOfImagePaths[self.currentImageIndex])
-		self.filename = self.fullpath.split('/')[-1]
-		extension = self.filename.split('.')[-1]
-		self.dirpath = self.fullpath[:-len(self.filename)]
-		try:
-			fullexif=self.currentImageOriginal._getexif()
-			self.dateAndTime = datetime.datetime.strptime(fullexif[36867], "%Y:%m:%d %H:%M:%S")
-		except Exception as ex:
-			print(ex)
-			print("EXIF problem! Using file timestamp...")
-			try:
-				self.dateAndTime = datetime.datetime.fromtimestamp(os.path.getmtime(self.fullpath))
-			except Exception as ex:	
-				print(ex)
-				print("No file timestamp!? Crashing...")
-				self.master.quit()
-				return None
-	
-		self.month = MONTHS[self.dateAndTime.month-1]
-		#check for existing month directory, create if not exists
-		dirContents = os.listdir(NUDADBDIR)
-		dirCheck = NUDADBDIR+self.month+str(self.dateAndTime.year)
-		if self.month+str(self.dateAndTime.year) in dirContents:
-			pass
-			#print(dirCheck+'/'+"  exists!")
-		else:
-			print("Creating "+dirCheck)
-			os.system("mkdir "+dirCheck)
-	
-		#copy file     filename = last six characters of hashstring
-		monthContents = os.listdir(NUDADBDIR+self.month+str(self.dateAndTime.year))
-		fullHash = self.getHash(self.fullpath)
-		self.newName = fullHash[-6:]+'.'+extension
-		if self.newName in monthContents:
-			print("COLLISION!     Skipping...")
-			#if using default import, move file from ./inbox/ to ./inbox/imported/
-			if os.path.isfile('./inbox/'+self.filename):
-				os.system("mv "+self.fullpath.replace(' ', "\ ")+" "+NUDADBDIR+"../inbox/skipped/"+self.newName)
-			if self.next_image():
-				self.tag_input()
+    def input_hist_next(self, event=None):
+        self.currentInputIndex += 1
+        if self.currentInputIndex < 0:
+            self.textbox.delete(0, tk.END)
+            self.textbox.insert(0, self.input_strings[self.currentInputIndex])
+        else:
+            self.currentInputIndex = 0
+            self.textbox.delete(0, tk.END)
 
-	def next_image(self, event=None):
-		if self.afterID is not None:
-			self.master.after_cancel(self.afterID)
-			self.afterID = None
-		self.currentImageIndex += 1
-		if self.currentImageIndex >= len(self.listOfImagePaths):
-			if self.showOrImport == 'show':
-				self.currentImageIndex = 0
-			elif self.showOrImport == 'import':
-				self.master.quit()
-				return False
-			else:
-				print("Something has gone very wrong...")
-				self.master.quit()
-				return False
-		try:
-			tempFileName = self.listOfImagePaths[self.currentImageIndex]
-			testOpen = Image.open(tempFileName)
-		except:
-			print("Can't open file: "+tempFileName+" (Not an image?) Skipping....")
-			if os.path.isfile('./inbox/'+self.filename):
-				os.system("mv "+self.fullpath.replace(' ', "\ ")+" "+NUDADBDIR+"../inbox/skipped/"+self.newName)
-			else:
-				print("Can't find file to skip!? Something has gone wrong...")
-			return False    #skip tag input for this skipped file
-		self.currentImage = ImageTk.PhotoImage(self.makeThumb(self.listOfImagePaths[self.currentImageIndex]))
-		self.showpanel.configure(image=self.currentImage)
-		self.showpanel.image = self.currentImage
-		return True
+    def send_tags(self, event=None):
+        self.currentInputIndex = 0
+        newTags = self.textbox.get()
+        if newTags in ['\\quit', '\\exit', '\\abort']:
+            self.master.quit()
+            return None
+        else:
+            self.input_strings.append(newTags)
+        taglist = self.input_strings[-1].split(' ')
+        tags = ','.join(taglist)
+        try:
+            os.system("cp "+self.fullpath.replace(' ', "\ ")+" "+NUDADBDIR+self.month+str(self.dateAndTime.year)+'/'+self.newName)
+            #Add entry to table
+            with open(NUDADBTABLE, 'a') as table:
+                table.write(self.newName+'\t'+'./nudaDBDir/'+self.month+str(self.dateAndTime.year)+'/'+'\t'+self.dateAndTime.strftime("%Y-%m-%d\t%H:%M:%S")+'\t'+tags+'\n')
+            #if using default import, move file from ./inbox/ to ./inbox/imported/
+            if os.path.isfile('./inbox/'+self.filename):
+                os.system("mv "+self.fullpath.replace(' ', "\ ")+" "+NUDADBDIR+"../inbox/imported/"+self.newName)
+        except:
+            print("copy problem!")
+        self.textbox.delete(0, tk.END)
+        if self.next_image():
+            self.tag_input()
 
-	def show_next(self, event=None):
-		self.next_image(event)
-		self.afterID = self.master.after(2000,self.show_next)
+    def tag_input(self, event=None):
+        #Get file data
+        self.fullpath = os.path.abspath(self.listOfImagePaths[self.currentImageIndex])
+        self.filename = self.fullpath.split('/')[-1]
+        extension = self.filename.split('.')[-1]
+        self.dirpath = self.fullpath[:-len(self.filename)]
+        try:
+            fullexif=self.currentImageOriginal._getexif()
+            self.dateAndTime = datetime.datetime.strptime(fullexif[36867], "%Y:%m:%d %H:%M:%S")
+        except Exception as ex:
+            print(ex)
+            print("EXIF problem! Using file timestamp...")
+            try:
+                self.dateAndTime = datetime.datetime.fromtimestamp(os.path.getmtime(self.fullpath))
+            except Exception as ex:    
+                print(ex)
+                print("No file timestamp!? Crashing...")
+                self.master.quit()
+                return None
+    
+        self.month = MONTHS[self.dateAndTime.month-1]
+        #check for existing month directory, create if not exists
+        dirContents = os.listdir(NUDADBDIR)
+        dirCheck = NUDADBDIR+self.month+str(self.dateAndTime.year)
+        if self.month+str(self.dateAndTime.year) in dirContents:
+            pass
+            #print(dirCheck+'/'+"  exists!")
+        else:
+            print("Creating "+dirCheck)
+            os.system("mkdir "+dirCheck)
+    
+        #copy file     filename = last six characters of hashstring
+        monthContents = os.listdir(NUDADBDIR+self.month+str(self.dateAndTime.year))
+        fullHash = self.getHash(self.fullpath)
+        self.newName = fullHash[-6:]+'.'+extension
+        if self.newName in monthContents:
+            print("COLLISION!     Skipping...")
+            #if using default import, move file from ./inbox/ to ./inbox/skipped/
+            if os.path.isfile('./inbox/'+self.filename):
+                os.system("mv "+self.fullpath.replace(' ', "\ ")+" "+NUDADBDIR+"../inbox/skipped/"+self.newName)
+            if self.next_image():
+                self.tag_input()
+            else:
+                self.master.quit()
+                return None
 
-	def show_stop(self, event):
-		self.master.quit()
 
-	def makeThumb(self, imagePath):
-		thumb = Image.open(imagePath)
-		self.currentImageOriginal = Image.open(imagePath)
-		try:
-			orientation = thumb._getexif()[0x0112]
-		except:
-			print("EXIF problem! Setting orientation=0")
-			orientation = 0
-		if orientation in self.rotations:
-			thumb = thumb.rotate(self.rotations[orientation], expand=1)
-		self.master.update_idletasks()
-		thumb.thumbnail((self.master.winfo_width(),self.master.winfo_height()-50))
-		return thumb
-		
+    def next_image(self, event=None):
+        if self.afterID is not None:
+            self.master.after_cancel(self.afterID)
+            self.afterID = None
+        self.currentImageIndex += 1
+        if self.currentImageIndex >= len(self.listOfImagePaths):
+            if self.showOrImport == 'show':
+                self.currentImageIndex = 0
+            elif self.showOrImport == 'import':
+                self.master.quit()
+                return False
+            else:
+                print("Something has gone very wrong...")
+                self.master.quit()
+                return False
+        try:
+            tempFileName = self.listOfImagePaths[self.currentImageIndex]
+            testOpen = Image.open(tempFileName)
+        except:
+            print("Can't open file: "+tempFileName+" (Not an image?) Skipping....")
+            if os.path.isfile('./inbox/'+self.filename):
+                os.system("mv "+self.fullpath.replace(' ', "\ ")+" "+NUDADBDIR+"../inbox/skipped/"+self.newName)
+            else:
+                print("Can't find file to skip!? Something has gone wrong...")
+            return False    #skip tag input for this skipped file
+        self.currentImage = ImageTk.PhotoImage(self.makeThumb(self.listOfImagePaths[self.currentImageIndex]))
+        self.showpanel.configure(image=self.currentImage)
+        self.showpanel.image = self.currentImage
+        return True
 
+    def show_next(self, event=None):
+        self.next_image(event)
+        self.afterID = self.master.after(2000,self.show_next)
+
+    def show_stop(self, event):
+        self.master.quit()
+
+    def makeThumb(self, imagePath):
+        thumb = Image.open(imagePath)
+        self.currentImageOriginal = Image.open(imagePath)
+        try:
+            orientation = thumb._getexif()[0x0112]
+        except:
+            print("EXIF problem! Setting orientation=0")
+            orientation = 0
+        if orientation in self.rotations:
+            thumb = thumb.rotate(self.rotations[orientation], expand=1)
+        self.master.update_idletasks()
+        thumb.thumbnail((self.master.winfo_width(),self.master.winfo_height()-50))
+        return thumb
