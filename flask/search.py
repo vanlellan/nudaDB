665746197993146cc208f22847d707f18241fd75
Randall Evan McClellan <vanlellan@gmail.com>
2022-06-08 22:37:43 -0500

    upgrade search.py to allow page flipping
    -add an integer on to the path for a search term to get that page of
    results

--- flask/search.py
+++ flask/search.py
@@ -17,22 +17,31 @@ DB_FOLDER = os.path.join('static','nudaDBDir')
 app = Flask(__name__, template_folder="templates")
 app.config['DB_FOLDER'] = DB_FOLDER
 
+numImages = 5
+
 @app.route('/')
 def home():
     #dummy home page
     return "Hello Search!"
 
 @app.route('/<keyword>')
-def search(keyword):
-    imageList = [f.strip("./nudaDBDir") for f in getImagesMatchingTags(keyword.split('-'))][:3]
+@app.route('/<keyword>/<int:page>')
+def search(keyword,page=None):
+    print("keyword = ", keyword)
+    print("page = ", page)
+    imageList = [f.strip("./nudaDBDir") for f in getImagesMatchingTags(keyword.split('-'))]
+    if page is None:
+        page = 1
+        full_filenames = [os.path.join(app.config['DB_FOLDER'], f) for f in imageList[numImages*(page-1):numImages*page]]
+    else:
+        full_filenames = [os.path.join("..", app.config['DB_FOLDER'], f) for f in imageList[numImages*(page-1):numImages*page]]
     #get first filenames
     #filenames = [f for f in os.listdir(SEARCH_FOLDER) if os.path.isfile(os.path.join(SEARCH_FOLDER, f))][:3]
     #full_filenames = [os.path.join(app.config['DUMMY_FOLDER'], f) for f in filenames]
     #return render_template("three-image.html", image0 = full_filenames[0], image1 = full_filenames[1], image2 = full_filenames[2])
     #return "The keyword is: "+keyword+"/n/n The images are: "+'\n'.join(imageList)
-    full_filenames = [os.path.join(app.config['DB_FOLDER'], f) for f in imageList]
-    print(full_filenames)
-    return render_template("three-image.html", image0 = full_filenames[0], image1 = full_filenames[1], image2 = full_filenames[2])
+    print("full_filenames = ", full_filenames)
+    return render_template("search.html", images = full_filenames)
 
 if __name__ == "__main__":
     app.run(debug=True, port=8080, host='0.0.0.0')
