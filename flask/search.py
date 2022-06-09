9c7c302221f64ac5210c8b0eaebf695b188f4e85
Randall Evan McClellan <vanlellan@gmail.com>
2022-06-08 23:14:13 -0500

    add prev and next links
    -also remove debug prints and old code

--- flask/search.py
+++ flask/search.py
@@ -27,21 +27,13 @@ def home():
 @app.route('/<keyword>')
 @app.route('/<keyword>/<int:page>')
 def search(keyword,page=None):
-    print("keyword = ", keyword)
-    print("page = ", page)
     imageList = [f.strip("./nudaDBDir") for f in getImagesMatchingTags(keyword.split('-'))]
     if page is None:
         page = 1
         full_filenames = [os.path.join(app.config['DB_FOLDER'], f) for f in imageList[numImages*(page-1):numImages*page]]
     else:
         full_filenames = [os.path.join("..", app.config['DB_FOLDER'], f) for f in imageList[numImages*(page-1):numImages*page]]
-    #get first filenames
-    #filenames = [f for f in os.listdir(SEARCH_FOLDER) if os.path.isfile(os.path.join(SEARCH_FOLDER, f))][:3]
-    #full_filenames = [os.path.join(app.config['DUMMY_FOLDER'], f) for f in filenames]
-    #return render_template("three-image.html", image0 = full_filenames[0], image1 = full_filenames[1], image2 = full_filenames[2])
-    #return "The keyword is: "+keyword+"/n/n The images are: "+'\n'.join(imageList)
-    print("full_filenames = ", full_filenames)
-    return render_template("search.html", images = full_filenames)
+    return render_template("search.html", images = full_filenames, keyword = keyword, prevthisnext = [page-1, page, page+1])
 
 if __name__ == "__main__":
     app.run(debug=True, port=8080, host='0.0.0.0')
